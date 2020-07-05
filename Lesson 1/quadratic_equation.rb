539a9097bd2431d651fad75a92da275289c5d36d
helgun <h.elgun@yahoo.com>
2020-07-05 22:46:05 +0400

    Update quadratic_equation.rb

--- Lesson 1/quadratic_equation.rb
+++ Lesson 1/quadratic_equation.rb
@@ -17,12 +17,12 @@ b = gets.to_f
 print 'c: '
 c = gets.to_f
 
-d = (b**2) - (4 * a * c)
+d = (b ** 2) - (4 * a * c)
 
 if d > 0
   d_root = Math.sqrt(d)
-  x1 = (-b + d_root)/(2 * a)
-  x2 = (-b - d_root)/(2 * a)
+  x1 = (-b + d_root) / (2 * a)
+  x2 = (-b - d_root) / (2 * a)
   puts "Дискриминант = #{d}, корень_1 = #{x1}, корень_2 = #{x2}"
 elsif d == 0
   x = -b/(2 * a)
