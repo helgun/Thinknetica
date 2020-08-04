aa09aa20c4a79337ea3f0a26fc9534c3cc4a63cd
Elgun <h.elgun@yahoo.com>
2020-08-04 18:26:50 +0400

    Updated

--- Lesson 4/railway/main.rb
+++ Lesson 4/railway/main.rb
@@ -3,16 +3,101 @@ require_relative 'route'
 require_relative 'station'
 require_relative 'cargo_train'
 
-cargo_train = CargoTrain.new("111", 2)
-puts cargo_train
+# cargo_train = CargoTrain.new("111", 2)
+# puts cargo_train
+#
+# train = Train.new("111", "passenger", 3)
+# train2 = Train.new("222", "пассажир", 3)
+# train3 = Train.new("333", "грузовой", 2)
+#
+# a_station = Station.new("a_station")
+# c_station = Station.new("c_station")
+#
+# route = Route.new(a_station, c_station)
+# train.set_route(route)
+# train.next_station
 
-train = Train.new("111", "passenger", 3)
-train2 = Train.new("222", "пассажир", 3)
-train3 = Train.new("333", "грузовой", 2)
+@stations = []
+@trains = []
+@route = []
 
-a_station = Station.new("a_station")
-c_station = Station.new("c_station")
+def create_station
+  puts "Enter the name of the station: "
+   station = gets.chomp
+   @stations << Station.new(station)
+   puts "Station #{station} created"
+end
 
-route = Route.new(a_station, c_station)
-train.set_route(route)
-train.next_station
+def create_train
+  #train type
+  puts "Choose train type: 1. passenger, 2. cargo"
+  train_type = gets.to_i
+  if train_type == 1
+    train_type = "passenger"
+  elsif train_type == 2
+    train_type = "cargo"
+  else
+    puts "Enter correct type"
+    create_train
+  end
+
+  puts "Enter train number"
+  train_number = gets.to_i
+  train = Train.new(train_number, train_type)
+  @trains << train
+end
+
+
+def routes
+  puts "1. Show routes"
+  puts "2. Add routes"
+  puts "3. Delete routes"
+  user_input = gets.to_i
+
+  if user_input == 1
+    @route.each { |route| puts route }
+  elsif user_input == 2
+    @stations.each { |station| puts "Station: #{station.name}"}
+    puts "Choose stations to create route"
+    user_station_input = ""
+
+    while user_station_input != "create"
+      route = []
+      user_station_input = gets.chomp
+      stations = @stations.map { |station| station.name }
+      if stations.include?(user_station_input)
+        puts "Added"
+        route << user_station_input
+        route.delete("create")
+        @route << route
+      else
+        puts "Station not found"
+        puts @route.to_s
+      end
+    end
+  elsif user_input == 3
+    #Delete route
+  end
+end
+
+
+
+loop do
+  puts "1. Create station"
+  puts "2. Create train"
+  puts "3. Routes"
+  puts "0. Exit"
+  user_input = gets.to_i
+  case user_input
+  when 1
+    create_station
+  when 2
+    create_train
+  when 3
+    routes
+  when 0
+    exit
+  else
+    puts "bye"
+  end
+end
