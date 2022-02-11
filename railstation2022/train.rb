af7057ed5ea54601d4a3ca2a4613d0035c41dc9b
helgun <h.elgun@yahoo.com>
2022-02-11 14:25:12 +0400

    updated

--- railstation2022/train.rb
+++ railstation2022/train.rb
@@ -1,23 +1,20 @@
 class Train
-  attr_reader :speed, :wagon_size, :current_station
+  attr_reader :speed, :wagon_size, :current_station, :type
 
-  TYPE = ["passenger", "freight"]
+  TYPE = ["passenger", "cargo"]
 
   def initialize(number, type, wagon_size)
     @number = number
-    @type = type if TYPE.include?(type)
+    @type = type
     @wagon_size = wagon_size
     @speed = 0
   end
 
   def increase_speed
-    puts "Increasing speed to 10..."
     @speed += 10
-    puts @speed
   end
 
   def stop
-    puts "Stopping..."
     @speed = 0
   end
 
@@ -29,22 +26,31 @@ class Train
     @wagon_size -= 1 if @speed == 0
   end
 
-  def accept_route(route)
-    @route = route
-    @current_station = route.first_station
+  def accept_route(way_stations)
+    @way_stations = way_stations
+    @current_station = way_stations.first_station
   end
 
   def next_station
-    puts "Going to the next station..."
-    current_station_index = @route.routes.index(@current_station)
-    @current_station = @route.routes[current_station_index + 1]
-    puts "Moved to #{@current_station.name}"
+    current_station_index = @way_stations.way_stations.index(@current_station)
+    next_station = @way_stations.way_stations[current_station_index + 1]
+    next_station
+  end
+
+  def move_next_station
+    current_station_index = @way_stations.way_stations.index(@current_station)
+    @current_station = @way_stations.way_stations[current_station_index + 1]
+  end
+
+  def move_prev_station
+    current_station_index = @way_stations.way_stations.index(@current_station)
+    @current_station = @way_stations.way_stations[current_station_index - 1] if current_station_index != 0
   end
 
   def prev_station
-    puts "Going to the prev station..."
-    current_station_index = @route.routes.index(@current_station)
-    @current_station = @route.routes[current_station_index - 1] if current_station_index != 0
-    puts "Moved to #{@current_station.name}"
+    current_station_index = @way_stations.way_stations.index(@current_station)
+    prev_station = @way_stations.way_stations[current_station_index - 1]
+    prev_station
   end
 end
+
